1b1264891fe50511cfd2ca26e9f3ba1f5e33e360
victorleiva <102170279+victorleiva@users.noreply.github.com>
2024-12-26 17:07:23 -0300

    Update Code-SQL-Riders_2020.sql

--- Code-SQL-Riders_2020.sql
+++ Code-SQL-Riders_2020.sql
@@ -38,18 +38,70 @@ ORDER BY
   ride_length2 ASC;
 
 #Creacion de una tabla donde se agrupan la cantidad de dias de la semana se repiten 
-CREATE TEMP TABLE Rides_2020 #Creacion de una tabla temporal para la creacion de una tabla de resultados separada
+CREATE TABLE IF NOT EXISTS `warm-aegis-419014.Case_Study_Cyclistic.Rides_members_2020`
 AS
-SELECT 
+SELECT
   day_of_week,
-  COUNT(day_of_week) AS total_days, #cuanta la cantidad de dias de la semana 
+  COUNT(day_of_week) AS total_days, #cuanta la cantidad de dias de la semana
+  start_station_name,
+  COUNT(start_station_name) AS start_station,
+  end_station_name,
+  COUNT(end_station_name) AS end_station,
+
   #Calculo de parametros generales de las duraciones de los viajes
-  MIN(ride_length_in_sec) AS min_ride,
-  MAX(ride_length_in_sec) AS max_ride,
-  AVG(ride_length_in_sec) AS mean_ride
+  MIN(ride_lenght_in_sec) AS min_ride,
+  MAX(ride_lenght_in_sec) AS max_ride,
+  AVG(ride_lenght_in_sec) AS mean_ride,
 FROM
-  `warm-aegis-419014.Case_Study_Cyclistic.Divvy_Trips_2020`
+  `warm-aegis-419014.Case_Study_Cyclistic.Rides_2020`
+WHERE
+  member_casual = 'member'
 GROUP BY
-  day_of_week #agrupa la cantidad de veces que se repite cada dia de la semana
+  start_station_name, end_station_name, day_of_week #agrupa la cantidad de veces que se repite cada dia de la semana
 ORDER BY
-  total_days DESC 
+  start_station DESC; 
+
+#WITH Rides_member_2019 AS (
+#  SELECT
+#    day_of_week,
+#    from_station_name,
+#    to_station_name,
+#    ROW_NUMBER() OVER (PARTITION BY COUNT(*) ORDER BY COUNT(*) DESC) AS total_viajes, #Ruta mas popular
+#    ROW_NUMBER() OVER (PARTITION BY day_of_week ORDER BY COUNT(*) DESC) AS ranking_dia, #Dias mas transitados
+#    ROW_NUMBER() OVER (PARTITION BY from_station_name ORDER BY COUNT(*) DESC) AS ranking_salida, #Estacion de salida mas popular
+#    ROW_NUMBER() OVER (PARTITION BY to_station_name ORDER BY COUNT(*) DESC) AS ranking_llegada #Estacion de llegada mas pooular
+#  FROM
+#    `warm-aegis-419014.Case_Study_Cyclistic.Rides_2019`
+#  WHERE
+#    member_casual = 'member'
+#  GROUP BY
+#    day_of_week, from_station_name, to_station_name
+#)
+#SELECT *
+#FROM Rides_member_2019
+#WHERE ranking_dia = 1 OR ranking_salida = 1 OR ranking_llegada = 1;
+
+
+#Creacion de una tabla donde se agrupan la cantidad de dias de la semana se repiten 
+CREATE TABLE IF NOT EXISTS `warm-aegis-419014.Case_Study_Cyclistic.Rides_casual_2020`
+AS
+SELECT
+  day_of_week,
+  COUNT(day_of_week) AS total_days, #cuanta la cantidad de dias de la semana
+  start_station_name,
+  COUNT(start_station_name) AS start_station,
+  end_station_name,
+  COUNT(end_station_name) AS end_station,
+
+  #Calculo de parametros generales de las duraciones de los viajes
+  MIN(ride_lenght_in_sec) AS min_ride,
+  MAX(ride_lenght_in_sec) AS max_ride,
+  AVG(ride_lenght_in_sec) AS mean_ride,
+FROM
+  `warm-aegis-419014.Case_Study_Cyclistic.Rides_2020`
+WHERE
+  member_casual = 'casual'
+GROUP BY
+  start_station_name, end_station_name, day_of_week #agrupa la cantidad de veces que se repite cada dia de la semana
+ORDER BY
+  start_station DESC;
